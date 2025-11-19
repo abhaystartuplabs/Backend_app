748cb8fdf75c617399d41aaa35a65e0ac7d82b34
Abhay Dubey <abhay.dubey@startuplabs.io>
2025-11-19 18:07:03 +0530

    Done 21

--- index.js
+++ index.js
@@ -6,12 +6,13 @@ const app = express();
 require('dotenv').config(); 
 const PORT = process.env.PORT || 8000;
 const connectToDb = require("./Config/db.js");
+const path = require("path");
 const cors = require('cors');
 require('./cron/scheduler.js')
 
 /* Handling cors problem */
 app.use(cors());
-
+app.use("/Public", express.static(path.join(__dirname, "Public")));
 app.use(express.json()); // Use express.json() instead of bodyParser.json()
 
 
